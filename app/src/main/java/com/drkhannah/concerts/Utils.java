4f5f87894c2abdea4117f7ed461919b56d9cfe4e
Derek Hannah <dhannah@thesoftwareguild.com>
2017-02-14 15:34:48 -0500

    fixed spelling error in default_artist_name string constant

--- app/src/main/java/com/drkhannah/concerts/Utils.java
+++ app/src/main/java/com/drkhannah/concerts/Utils.java
@@ -14,7 +14,7 @@ public class Utils {
     public static String getSharedPrefsArtistName(Context context) {
         //get the artist name saved in the com.drkhannah.concerts.CONCERTS_SHARED_PREFERENCE_FILE Shared Preferences file
         SharedPreferences sharedPref = context.getSharedPreferences(context.getString(R.string.preference_file_key), MODE_PRIVATE);
-        return sharedPref.getString(context.getString(R.string.shared_prefs_artist_name), context.getString(R.string.default_artsit_name));
+        return sharedPref.getString(context.getString(R.string.shared_prefs_artist_name), context.getString(R.string.default_artist_name));
     }
 
     public static void saveSharedPrefsArtistName(Context context, String artistName) {
