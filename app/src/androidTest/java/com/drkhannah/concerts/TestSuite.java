a59ad6732d94b691e33aac7906891971e6cc52c7
Derek Hannah <dhannah@thesoftwareguild.com>
2017-02-24 15:54:47 -0500

    deleted GetConcertsTask.java and the TestGetConcertsTask.java and removed it from the test suite

--- app/src/androidTest/java/com/drkhannah/concerts/TestSuite.java
+++ app/src/androidTest/java/com/drkhannah/concerts/TestSuite.java
@@ -19,7 +19,7 @@ import org.junit.runners.Suite;
         TestUriMatcher.class,
         TestConcertsContract.class,
         TestConcertsProvider.class,
-        TestGetConcertsTask.class})
+        })
 
 public class TestSuite {}
 
